8de183f042e0912c8b9fdb92c68f98ad18a12ec2
Clayton Ferreira <146362136+cleyton13fs@users.noreply.github.com>
2024-07-30 12:02:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('searchBtn').addEventListener('click', function() {
+    const username = document.getElementById('username').value;
+    if (username) {
+        fetch(`https://api.github.com/users/${username}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.message === "Not Found") {
+                    alert("Usuário não encontrado!");
+                } else {
+                    document.getElementById('avatar').src = data.avatar_url;
+                    document.getElementById('avatar').style.display = 'block';
+                    document.getElementById('name').textContent = data.name || 'Nome não disponível';
+                    document.getElementById('bio').textContent = data.bio || 'Biografia não disponível';
+                }
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+                alert('Ocorreu um erro ao buscar as informações do usuário.');
+            });
+    } else {
+        alert("Por favor, insira um nome de usuário.");
+    }
+});
