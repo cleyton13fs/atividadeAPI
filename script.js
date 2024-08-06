a19ee34e564246b4296ad9c8f848823161c78c5b
Clayton Ferreira <146362136+cleyton13fs@users.noreply.github.com>
2024-08-06 10:52:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('searchBtn').addEventListener('click', function() {
     const username = document.getElementById('username').value;
-    const token ='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'
+    const token = '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';
+
     if (username) {
         fetch(`https://api.github.com/users/${username}`)
             .then(response => response.json())
@@ -12,6 +13,24 @@ document.getElementById('searchBtn').addEventListener('click', function() {
                     document.getElementById('avatar').style.display = 'block';
                     document.getElementById('name').textContent = data.name || 'Nome não disponível';
                     document.getElementById('bio').textContent = data.bio || 'Biografia não disponível';
+
+                    // Fetch repositories
+                    fetch(`https://api.github.com/users/${username}/repos`)
+                        .then(response => response.json())
+                        .then(repos => {
+                            const reposList = document.getElementById('repos');
+                            reposList.innerHTML = '';
+
+                            repos.forEach(repo => {
+                                const repoItem = document.createElement('li');
+                                repoItem.textContent = repo.name;
+                                reposList.appendChild(repoItem);
+                            });
+                        })
+                        .catch(error => {
+                            console.error('Erro ao buscar repositórios:', error);
+                            alert('Ocorreu um erro ao buscar os repositórios do usuário.');
+                        });
                 }
             })
             .catch(error => {
